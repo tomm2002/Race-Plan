f460bcb7ed2c0772932c308b64373a8d0fb0cbc5
Tomaž <Toma@DESKTOP-VT7MOHK>
2024-10-06 19:28:26 +0200

    email requred not jsut name

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,18 +2,21 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Note
 
-
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}} #only accept password, not return it
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        print(validated_data)
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
-
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/api/views.py
+++ backend/api/views.py
@@ -33,4 +33,4 @@ class NoteDelete(generics.DestroyAPIView):
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [AllowAny] #allow even not auth users to create new users
+    permission_classes = [AllowAny]  # allow even not auth users to create new users
